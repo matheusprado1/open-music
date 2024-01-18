f0980d047760c0b13b7e2bbee82d60edd7640181
matheusprado1 <matheusprado1096@gmail.com>
2024-01-18 14:27:00 -0300

    feat: add export

--- src/scripts/productsData.js
+++ src/scripts/productsData.js
@@ -1,4 +1,4 @@
-const products = [
+export const products = [
   {
     title: "Magnetite",
     category: 8,
@@ -37,7 +37,7 @@ const products = [
   },
 ];
 
-const categories = [
+export const categories = [
   "Todos",
   "Pop",
   "MPB",
